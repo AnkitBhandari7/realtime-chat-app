bff5f9e149fb3f1093bc7a06e87a760b369097c3
ankeet780 <ankeetbhandari780@gmail.com>
2025-10-15 12:13:29 +0545

    Fix CORS configuration for Vercel frontend

--- server/src/app.ts
+++ server/src/app.ts
@@ -9,11 +9,12 @@ const app = express();
 
 app.use(helmet());
 app.use(express.json());
+
 app.use(
   cors({
     origin: [
       "http://localhost:5173", // for local dev (Vite)
-      "https://realtime-chat-il7zko648-ankit-bhandaris-projects-5cd522fa.vercel.app/login"
+      "https://realtime-chat-il7zko648-ankit-bhandaris-projects-5cd522fa.vercel.app" // 
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
