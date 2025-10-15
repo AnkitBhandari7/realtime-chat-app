e0e9db00ba47f27f1c5f3e0f3dfe9326b75584b6
ankeet780 <ankeetbhandari780@gmail.com>
2025-10-15 11:29:41 +0545

    latest update

--- server/dist/routes/chatRoutes.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { authenticate } from "../middleware/auth.js";
-import { getMessages, getStats } from "../controllers/chatController.js";
-const router = Router();
-router.get("/messages", authenticate, getMessages);
-router.get("/stats", authenticate, getStats);
-export default router;
